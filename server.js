3229ecedbaba5f75ec82827ea2162e074dab9c4c
Thomas Modolo <thomas.modolo@ynov.com>
2022-03-02 12:02:17 +0100

    feat : quick sample of express js with test entry

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/api/test', (req, res) => {
+    console.log('ça passe mon GA')
+    res.send('t pd')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
